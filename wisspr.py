4f5f4d2d06c3cdc4aeb08ca4b52c1fb6556a3873
Nitesh Menon <nsm2134@columbia.edu>
2014-02-04 20:01:10 -0500

    almost finished a working signin/signout/signup system

--- wisspr.py
+++ wisspr.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, request
+import requests
+from models import db, User
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -20,15 +21,28 @@ def testdb():
 @app.route('/login', methods=["GET", "POST"])
 def login():
 	error = None
-	if request.method == "POST":
-		if authenticate(request.form['user'], request.form['password']):
-			return request.form['user']
+	if request.method == 'POST':
+		if authenticate(request.form['email'], request.form['password']):
+			return "OK"
 		else:
 			error = 'Invalid username/password'
 
 	return render_template('home.html', error=error)
 
-#def authenticate(user, password):
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+	if request.method == 'GET':
+		return render_template('signup.html')
+	elif request.method == 'POST':
+		user = User.query.filter_by(email = request.form['email']).first()
+		if user:
+			return render_template('signup.html', error=
+				"That email is already taken")
+		else:
+			return "SIGNED UP!"
+
+def authenticate(user, password):
+	return false
 	#encrypted_password = encrypt(password)
 	#if 
 
